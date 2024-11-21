e45cfefce8685d4051c341fc1f18b916ccc84fd6
Venkat Krishna <codekvkk@gmail.com>
2024-11-21 16:26:14 +0530

    bool again

--- boolean.cpp
+++ boolean.cpp
@@ -6,9 +6,13 @@ int main()
 {
     bool toss = true;
     bool boss =false;
-    cout<<toss;
-    cout<<boss;
+    cout<<toss<<endl;
+    cout<<boss<<endl;
+    //boolean variables output values 0/1
 
+    int x = 10,y=20;
+    cout<<(x>y)<<endl;
+    //boolean statement 1 = true 0 = false
     
     return 0;
 }
